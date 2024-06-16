4f2983a98b5f53dce001429f94ef0a8ed3d52608
rivka-yuliia <juliacreadora@gmail.com>
2024-06-16 17:26:22 +0300

    item model

--- app/medium/items.py
+++ app/medium/items.py
@@ -1,12 +1,13 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
+"""
+Item model for the posts.
+"""
 
 import scrapy
 
+from itemloaders.processors import Join
+
 
 class MediumItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field(output_processor=Join())
+    excerpt = scrapy.Field(output_processor=Join())
+    link = scrapy.Field(output_processor=Join())
