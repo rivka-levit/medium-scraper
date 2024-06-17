b2923bbf7c756d614bea7662365d87a2778347ff
rivka-yuliia <juliacreadora@gmail.com>
2024-06-17 16:22:50 +0300

    parse function

--- app/medium/spiders/posts.py
+++ app/medium/spiders/posts.py
@@ -1,4 +1,12 @@
 import scrapy
+import time
+
+from scrapy.loader import ItemLoader
+from scrapy.selector import Selector
+
+from selenium import webdriver
+
+from medium.items import MediumItem
 
 
 class PostsSpider(scrapy.Spider):
@@ -6,5 +14,58 @@ class PostsSpider(scrapy.Spider):
     allowed_domains = ["medium.com"]
     start_urls = ["https://medium.com"]
 
-    def parse(self, response, **kwargs):
-        pass
+    def parse(self, r, **kwargs):
+        options = webdriver.ChromeOptions()
+        # options.headless = True
+        driver = webdriver.Remote(
+            command_executor='http://chrome:4444',
+            options=options
+        )
+        driver.get(self.start_urls[0])
+        driver.implicitly_wait(5)
+
+        i = 1
+        num_scrolls = 10
+        last_height = driver.execute_script(
+            "return document.body.scrollHeight"
+        )
+
+        while True and i <= num_scrolls:
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);"
+            )
+            time.sleep(3)
+            new_height = driver.execute_script(
+                "return document.body.scrollHeight"
+            )
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+            response = driver.page_source
+            selector = Selector(text=response)
+            containers = selector.xpath('//main//div[@class="gb lk ll lm"]')
+
+            for c in containers:
+                item = ItemLoader(
+                    item=MediumItem(),
+                    response=response,
+                    selector=c
+                )
+                item.add_xpath(
+                    'title',
+                    './/h2/text()'
+                )
+                item.add_xpath(
+                    'excerpt',
+                    './/div[@class="h k ng ej eo"]/p/text()'
+                )
+                item.add_xpath(
+                    'link',
+                    './/div[@class="ly s"]//a/@href'
+                )
+                yield item.load_item()
+
+            i += 1
+
+        driver.quit()
